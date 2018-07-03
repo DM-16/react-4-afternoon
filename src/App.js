ec555491fbc275623cd0458707755d8116a69287
jdaniel <jdricher2017@gmail.com>
2018-07-03 15:52:15 -0500

    finished

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'; //allows us to add clickable links into the DOM
+//so the user can navigate the application
+import routes from './routes';
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <nav className='nav'>
-          <div>WestSide University</div> 
+          <div>WestSide University</div>
           <div className='link-wrap'>
-              <div className='links'>Home</div>
-              <div className='links'>About</div> 
+            <Link to="/" className='links'>Home</Link>
+            <Link to="/about" className='links'>About</Link>
           </div>
         </nav>
+        {routes}
       </div>
     )
   }
